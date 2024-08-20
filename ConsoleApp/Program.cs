d020fd29de8eb5cc9c7e5f27a3a9f3755227d706
Andres Gaviria <Andres.Gaviria@sleepgroupsolutions.com>
2024-08-20 09:58:39 -0500

    Clase 20-08-2024

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,65 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var producto = new Productos(1, "TV", 2000000, 5);
+Console.WriteLine(producto.get_nombre());
+
+public abstract class ProductosBase1
+{
+    public abstract bool Valor();
+}
+
+public interface ProductosBase2
+{
+    double Valor();
+}
+
+public class Productos : ProductosBase2
+{
+    // Atributos
+    private int id = 0;
+    private string nombre = "";
+    private double precio = 0.0;
+    private double cant = 0.0;
+
+    // Contructor
+    public Productos(int id, string nombre, double precio, double cant)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cant = cant;
+    }
+
+    // Propiedades
+    public int get_id() { return id; }
+    public string get_nombre() { return nombre; }
+    public void set_nombre(string valor) { nombre = valor; }
+
+    // Metodos
+    public double Valor()
+    {
+        return 16;
+    }
+}
+
+public class Productos2 : ProductosBase2
+{
+    // Atributos
+    private int id = 0;
+    private string nombre = "";
+    private double precio = 0.0;
+    private double cant = 0.0;
+
+    // Contructor
+    public Productos2(int id, string nombre, double precio, double cant)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cant = cant;
+    }
+
+    // Metodos
+    public double Valor()
+    {
+        return 15;
+    }
+}
--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d72ba27b861b0f6b5193b1c813b11e71d9e8847")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337e68a5c9290f013302032d444f5cc820e029e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
