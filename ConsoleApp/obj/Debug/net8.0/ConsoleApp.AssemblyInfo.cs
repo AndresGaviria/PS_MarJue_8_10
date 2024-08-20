337e68a5c9290f013302032d444f5cc820e029e7
Andres Gaviria <Andres.Gaviria@sleepgroupsolutions.com>
2024-08-19 21:32:14 -0500

    Remover comentarios

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+573a2a3f1949dea7d827e38ab4d08e546d8d5b5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d72ba27b861b0f6b5193b1c813b11e71d9e8847")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
